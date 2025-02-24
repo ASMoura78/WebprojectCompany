cdf5e4a20d582c0cdf832c606828792e46945160
Alan Soares <asmoura1@gmail.com>
2025-02-24 19:35:24 -0300

    criação do aruivo web.xml

--- web/login.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.querySelector('form');
-
-    loginForm.addEventListener('submit', (event) => {
-        // O formulário será enviado ao servidor para validação no servlet LoginServlet
-    });
-});
-
-
